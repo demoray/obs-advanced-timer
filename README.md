3adc90396340760a336e24ebc50291f49741e3d1
Brian Caswell <bcaswell@microsoft.com>
2023-09-29 05:17:19 -0400

    simplify meeting costs implementation

--- README.md
+++ README.md
@@ -1,43 +1,20 @@
-# obs-advanced-timer
+# obs-meeting-costs
+This is a Lua script for OBS Studio that sets a text source as a cost calculator.
 
-This is a Lua script for OBS Studio that sets a text source as a timer with advanced options.  
+Based off of [obs-advanced-timer](https://github.com/cg2121/obs-advanced-timer).
 
-**Modes**  
-- Countdown (countdown from specified amount of seconds)  
-- Countup (starts a stopwatch timer)  
-- Specific time (starts to countdown to a specific time, such as countdown to 12:00 am)  
-- Specific date & time (starts to countdown to a date and time)  
-- Streaming timer (starts timer when streaming starts)  
-- Recording timer (starts timer when recording start)  
-- With the countdown modes, there is an option to countup when the countdown is finished
-- Meeting Costs (how much has this meeting cost thus far?)
+## Configuration
+* Initial Cost: Specify the initial cost for the meeting (Default: 0.0)
+* Average hourly rate: Average salary per hour for the participants (Default: 15.00)
+* Number of participants: How many people are currently in the meeting (Default: 1)
+* Show the participant count: Should the number of participants be show in the text (Default: false)
+* Text prefix: What text to show before the dollar amount (Default: "This meeting has cost") 
+* Text suffix: What text to show after the dollar amount (Default: "thus far.")
+* Text source: Name of the text source in OBS
 
-**Formatting**  
-The default format is: %0H:%0m:%0s (00:00:00)  
-
-```
-%d - days
-%0h - hours with leading zero (00..23)
-%h - hours (0..23)
-%0H - hours with leading zero (00..infinity)
-%H - hours (0..infinity)
-%0m - minutes with leading zero (00..59)
-%m - minutes (0..59)
-%0M - minutes with leading zero (00..infinity)
-%M - minutes (0..infinity)
-%0s - seconds with leading zero (00..59)
-%s - seconds (0..59)
-%0S - seconds with leading zero (00..infinity)
-%S - seconds (0..infinity)
-%t - tenths
-%2t - hundredths
-%3t - thousandths
-```
-
-**Activation Modes**  
-- Global (the timer is always active)  
-- Start timer on activation (starts timer when source is activated, such as when switching to a scene with that source or turning the visibility of the source to on)  
-
-
-**Hotkeys**  
-Hotkeys can be set for starting/stopping and to the reset timer.
+## Hotkeys
+The following hotkeys can be registered:
+* Reset Timer
+* Pause/Resume
+* Increment meeting participants
+* Decrement meeting participants
---
+++ meeting-timer.lua
@@ -0,0 +1,185 @@
+obs           = obslua
+activated     = false
+current_spend = 0.0
+hourly_rate   = 15.0
+initial_cost  = 0.0
+last_time 	  = 0
+participants  = 1
+source_name   = ""
+timer_active  = false
+
+hotkey_id_dec_participants = obs.OBS_INVALID_HOTKEY_ID
+hotkey_id_inc_participants = obs.OBS_INVALID_HOTKEY_ID
+hotkey_id_pause     = obs.OBS_INVALID_HOTKEY_ID
+hotkey_id_reset     = obs.OBS_INVALID_HOTKEY_ID
+
+function script_description()
+	return "How much has this meeting cost?"
+end
+
+function set_cost_text(ns)
+	if ns > 0 then
+		local ms = math.floor(ns/ 1000000)
+		local current_cost = ms / 3600000 * participants * hourly_rate
+		current_spend = current_spend + current_cost
+	end
+	local text = string.format("This meeting has cost $%.02f thus far (%d participants)", current_spend, participants)
+
+	local source = obs.obs_get_source_by_name(source_name)
+	if source ~= nil then
+		local settings = obs.obs_data_create()
+		obs.obs_data_set_string(settings, "text", text)
+		obs.obs_source_update(source, settings)
+		obs.obs_data_release(settings)
+		obs.obs_source_release(source)
+	end
+end
+
+function script_tick(sec)
+	if timer_active == false then
+		return
+	end
+
+	local since = 0
+	local now = obs.os_gettime_ns()
+	if last_time > 0 then
+		since = now - last_time
+	end
+	last_time = now
+	
+	set_cost_text(since)
+end
+
+function start_timer()
+	timer_active = true
+	last_time = obs.os_gettime_ns()
+end
+
+function stop_timer()
+	timer_active = false
+end
+
+function reset(pressed)
+	if pressed == true then
+		current_spend = initial_cost
+		last_time = 0
+		set_cost_text(0)
+	end
+end
+
+function inc_participants(pressed)
+	if pressed == true then
+		participants = participants + 1
+	end
+	set_cost_text(0)
+end
+
+function dec_participants(pressed)
+	if pressed == true then
+		participants = participants - 1
+		if participants < 0 then
+			participants = 0
+		end
+	end
+	set_cost_text(0)
+end
+
+function on_pause(pressed)
+	if pressed == true then
+    	if timer_active then
+    		stop_timer()
+    	else
+    		start_timer()
+    	end
+    end
+	set_cost_text(0)
+end
+
+function pause_button_clicked(props, p)
+	on_pause(true)
+	return true
+end
+
+function reset_button_clicked(props, p)
+	reset(true)
+	return true
+end
+
+function script_properties()
+	local props = obs.obs_properties_create()
+	
+	obs.obs_properties_add_float(props, "initial_cost", "Initial Cost", 0, 100000000, 0)
+	obs.obs_properties_add_float(props, "hourly_rate", "Average hourly rate", 1, 100000000, 1)
+	obs.obs_properties_add_int(props, "participants", "Number of participants", 1, 100000000, 1)
+	local p = obs.obs_properties_add_list(props, "source", "Text source", obs.OBS_COMBO_TYPE_EDITABLE, obs.OBS_COMBO_FORMAT_STRING)
+	local sources = obs.obs_enum_sources()
+	if sources ~= nil then
+		for _, source in ipairs(sources) do
+			source_id = obs.obs_source_get_unversioned_id(source)
+			if source_id == "text_gdiplus" or source_id == "text_ft2_source" then
+				local name = obs.obs_source_get_name(source)
+				obs.obs_property_list_add_string(p, name, name)
+			end
+		end
+	end
+	obs.source_list_release(sources)
+
+	obs.obs_properties_add_button(props, "pause_button", "Start/Stop", pause_button_clicked)
+	obs.obs_properties_add_button(props, "reset_button", "Reset", reset_button_clicked)
+
+	return props
+end
+
+function script_update(settings)
+	source_name = obs.obs_data_get_string(settings, "source")
+	participants = obs.obs_data_get_int(settings, "participants")
+	hourly_rate = obs.obs_data_get_double(settings, "hourly_rate")
+	initial_cost = obs.obs_data_get_double(settings, "initial_cost")
+end
+
+function script_defaults(settings)
+	obs.obs_data_set_default_int(settings, "offset", 0)
+	obs.obs_data_set_default_int(settings, "participants", 1)
+	obs.obs_data_set_default_double(settings, "hourly_rate", 15.0)
+	obs.obs_data_set_default_double(settings, "initial_cost", 0.0)
+end
+
+function script_save(settings)
+	local hotkey_save_array_reset = obs.obs_hotkey_save(hotkey_id_reset)
+	local hotkey_save_array_pause = obs.obs_hotkey_save(hotkey_id_pause)
+	local hotkey_save_array_inc_participants = obs.obs_hotkey_save(hotkey_id_inc_participants)
+	local hotkey_save_array_dec_participants = obs.obs_hotkey_save(hotkey_id_dec_participants)
+	obs.obs_data_set_array(settings, "reset_hotkey", hotkey_save_array_reset)
+	obs.obs_data_set_array(settings, "pause_hotkey", hotkey_save_array_pause)
+	obs.obs_data_set_array(settings, "increment_participants", hotkey_save_array_inc_participants)
+	obs.obs_data_set_array(settings, "decrement_participants", hotkey_save_array_dec_participants)
+	obs.obs_data_array_release(hotkey_save_array_pause)
+	obs.obs_data_array_release(hotkey_save_array_reset)
+	obs.obs_data_array_release(hotkey_save_array_inc_participants)
+	obs.obs_data_array_release(hotkey_save_array_dec_participants)
+end
+
+function script_load(settings)
+	local sh = obs.obs_get_signal_handler()
+	obs.signal_handler_connect(sh, "source_activate", source_activated)
+	obs.signal_handler_connect(sh, "source_deactivate", source_deactivated)
+
+	hotkey_id_reset = obs.obs_hotkey_register_frontend("reset_timer", "Reset Timer", reset)
+	hotkey_id_pause = obs.obs_hotkey_register_frontend("pause_timer", "Pause/Resume", on_pause)
+	hotkey_id_inc_participants = obs.obs_hotkey_register_frontend("increment_participants", "increment meeting participants", inc_participants)
+	hotkey_id_dec_participants = obs.obs_hotkey_register_frontend("decrement_participants", "decrement meeting participants", dec_participants)
+	local hotkey_save_array_reset = obs.obs_data_get_array(settings, "reset_hotkey")
+	local hotkey_save_array_pause = obs.obs_data_get_array(settings, "pause_hotkey")
+	local hotkey_save_array_inc_participants = obs.obs_data_get_array(settings, "increment_participants")
+	local hotkey_save_array_dec_participants = obs.obs_data_get_array(settings, "decrement_participants")
+	obs.obs_hotkey_load(hotkey_id_reset, hotkey_save_array_reset)
+	obs.obs_hotkey_load(hotkey_id_pause, hotkey_save_array_pause)
+	obs.obs_hotkey_load(hotkey_id_inc_participants, hotkey_save_array_inc_participants)
+	obs.obs_hotkey_load(hotkey_id_dec_participants, hotkey_save_array_dec_participants)
+	obs.obs_data_array_release(hotkey_save_array_reset)
+	obs.obs_data_array_release(hotkey_save_array_pause)
+	obs.obs_data_array_release(hotkey_save_array_inc_participants)
+	obs.obs_data_array_release(hotkey_save_array_dec_participants)
+
+	script_update(settings)
+end
